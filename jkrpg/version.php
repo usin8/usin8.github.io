1954ec90d9caef4ea853fac6d5207146f09ae047
Yusin <35628242+usin8@users.noreply.github.com>
2025-06-02 20:24:32 +0900

    Create version.php

---
+++ jkrpg/version.php
@@ -0,0 +1,10 @@
+<?php
+  $data = '{
+  "version" : "13",
+  "downlink" : "https://drive.usercontent.google.com/download?id=1aJyncGmlV5eBfrprIOPm-WobUO6y15bN&export=download&authuser=0&confirm=t&uuid=e077505d-db41-4a3f-83e0-4a6b2e1efbbc&at=AO7h07d-LwwYeioChG1ytV7YxBmd%3A1725982890147"
+  }';
+  $character = json_decode($data);
+  $qwer = json_decode("d5294ac54a1018bd2f34635ebd667c91ce896832524f94e9143b355238d88c5cc0b81a7b4440bf3eeyAidmVyc2lvbiI6ICIxIiwgImRvd25saW5rIjogImh0dHBzOlwvXC9lbmpveWprcnBncmlnaHRub3dhZ2Fpbi5vbi5kcnYudHdcL2prdXBkYXRlXC9pbnN0YWxsZXIucGhwIiB9AA==");
+  echo $data;
+  
+?>
